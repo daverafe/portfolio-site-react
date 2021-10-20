126594af68d437bd3bdec2fa2fab33ee5d1ad12c
David Rafe <david.rafe@gmail.com>
2021-10-20 13:41:12 -0400

    added ids to divs, started styling with where they will appear on the page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const queue = {
     description: "A TV/Movie/Media recommendation app where users can create an account, and recommend content to other users that will populate on their profiles. "
 }
 const lifestyleRoster =  {
-    title: "Lifetsyle Roster",
+    title: "Lifestyle Roster",
     image: "https://cdn.iconscout.com/icon/free/png-128/javascript-1-225993.png",
     url: "https://vigorous-hoover-20c685.netlify.app/",
     description: "Pinterest / Registry style app that allows you to set up categorized rosters of all your favorite items from across the web, creating gift or lifestyle inspiration. "
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
 
 function ProjectContainer({projectInfo}) {
     return (
-        <div>
-            <Link to={projectInfo.url}><img src={projectInfo.image} alt="tech icon"/></Link>
-            <Link to={projectInfo.url}><h2>{projectInfo.title}</h2></Link>
-            <p>{projectInfo.description}</p>
+        <div id={projectInfo.title.toLowerCase()}>
+            <Container>
+                <Link to={projectInfo.url}><img src={projectInfo.image} alt="tech icon"/></Link>
+                <Link to={projectInfo.url}><h2>{projectInfo.title}</h2></Link>
+                <p>{projectInfo.description}</p>
+            </Container>
         </div>
     )
 }
