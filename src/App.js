e511f336942a5a132541f454d9029064be55bdf0
David Rafe <david.rafe@gmail.com>
2021-10-21 11:57:35 -0400

    added bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import ProjectContainer from './components/ProjectContainer';
 import AboutContainer from './components/AboutContainer';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <Router>
       <div className="App">
+      <NavBar/>
       <Switch>
         <Route exact path="/" component={ProjectContainer}/>
         <Route exact path="/about" component={AboutContainer}/>
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+function NavBar() {
+    return (
+        <>
+        <Navbar sticky="top" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    </Nav>
+            </Container>
+        </Navbar>
+    </>
+    )
+}
+
+export default NavBar
