250c5e72747706bfd18798f90ec8977b49be4939
David Rafe <david.rafe@gmail.com>
2021-10-28 14:47:04 -0400

    updated design of homepage to incorperate more CSS elements

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
 
 function Project({projectInfo}) {
     return (
         <div id={projectInfo.title.toLowerCase()}>
-            <Container>
-                <Link to={projectInfo.url}><img src={projectInfo.image} alt="tech icon"/></Link>
-                <Link to={projectInfo.url}><h2>{projectInfo.title}</h2></Link>
-                <p>{projectInfo.description}</p>
-                <p>{projectInfo.technologies}</p>
-            </Container>
+            <Link to={projectInfo.url}><img src={projectInfo.image} alt="tech icon"/></Link>
+            <Link to={projectInfo.url}><h2>{projectInfo.title}</h2></Link>
+            <h4>{projectInfo.description}</h4>
+            <h5>{projectInfo.technologies}</h5>
         </div>
     )
 }
