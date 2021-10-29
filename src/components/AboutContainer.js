11a8df618f88c28f5883c8c4c3cc7d210167f8fc
David Rafe <david.rafe@gmail.com>
2021-10-29 16:28:35 -0400

    updated design of links on about page

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -6,6 +6,7 @@ function AboutContainer() {
             <img src="https://avatars.githubusercontent.com/u/42283106?v=4" alt="profile"/>
             <h2>David Rafe</h2>
             <h4>I love learning every single day, and the most exciting part is that my journey with code has just begun. I've been able to quickly pick up concepts such as Object-Orientation, and MVC style responsibilities utilizing Ruby on Rails and Javascript(ES6). I've been able to incorporate frontend frameworks and technologies such as React and Redux, in conjunction with a Rails backend setup, in a matter of weeks. I am excited to continue to explore what else is out there, and what I can do with it.</h4>
+            <br/>
             <a href="https://www.linkedin.com/in/david-rafe/" target="_blank" rel="noreferrer"><h5>LinkedIn</h5></a>
             <a href="https://github.com/daverafe" target="_blank" rel="noreferrer"><h5>Github</h5></a>
             <a href="https://david-rafe.medium.com/" target="_blank" rel="noreferrer"><h5>Blog</h5></a>
