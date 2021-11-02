eded4ef2ae3b805c00288728b807084cf9a2a033
David Rafe <david.rafe@gmail.com>
2021-11-02 11:58:51 -0400

    added description and buttons to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
 function Homepage() {
     return (
         <div>
             <h1>Welcome To My Portfolio Site</h1>
+            <h3>Frontend software engineer new to the industry</h3>
+            <h3>Check out my projects and find out a little more about me here</h3>
+            <Link to="/projects">
+                <Button variant="secondary">Projects</Button>{' '}
+            </Link>
+            <Link to="/about">
+                <Button variant="secondary">About</Button>{' '}
+            </Link>
         </div>
     )
 }
