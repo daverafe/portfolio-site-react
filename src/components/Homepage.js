e823d2fdf88c28000994788c33465f40d88e34f8
David Rafe <david.rafe@gmail.com>
2021-11-04 17:47:50 -0400

    added images to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
+import {Button, Container, Row, Col} from 'react-bootstrap'
 
 function Homepage() {
     return (
@@ -14,6 +14,20 @@ function Homepage() {
             <Link to="/about">
                 <Button variant="secondary">About</Button>{' '}
             </Link>
+            <Container>
+                <Row>
+                    <Col>
+                        <img alt="html" src="https://images.unsplash.com/photo-1621839673705-6617adf9e890?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aHRtbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
+                    </Col>
+                    <Col>
+                        <img alt="css" src="https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3NzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
+                    </Col>
+                    <Col>
+                        <img alt="javascript" src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGphdmFzY3JpcHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
+                    </Col>
+                </Row>
+            </Container>
+            <h5>This site was built with React</h5>
         </div>
     )
 }
