d2c2e4c41f63e128d57cf19bfef11257ad35167a
David Rafe <david.rafe@gmail.com>
2021-11-08 17:15:17 -0500

    added glowing border on hover for project imgs

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Project({projectInfo}) {
     return (
         <div id={projectInfo.title.toLowerCase()}>
-            <a href={projectInfo.url} target="_blank" rel="noreferrer"><img src={projectInfo.image} alt="tech icon"/></a>
+            <a href={projectInfo.url} target="_blank" rel="noreferrer"><img className="glowing-border" src={projectInfo.image} alt="tech icon"/></a>
             <a href={projectInfo.url} target="_blank" rel="noreferrer"><h2>{projectInfo.title}</h2></a>
             <h4>{projectInfo.description}</h4>
             <h5>{projectInfo.technologies}</h5>
