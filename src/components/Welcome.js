ec010c89fbaf43da6d8136e0f496152290be3068
David Rafe <david.rafe@gmail.com>
2021-11-17 12:29:05 -0500

    updated div styling for welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,10 +3,10 @@ import {Button} from 'react-bootstrap'
 
 function Welcome() {
     return (
-        <div>
+        <div id="welcome">
             <h1>Welcome To My Portfolio Site</h1>
             <h3>Click To Enter</h3>
-            <Button>Enter</Button>
+            <Button variant="secondary">Enter</Button>
         </div>
     )
 }
